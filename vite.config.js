ecdc981a15808c2e16a22727d8a4ab0c1dbc1bd9
LixinGao <1187776469@qq.com>
2023-05-15 21:14:55 +0800

    element 按需导入配置

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,23 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+// element-plus
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+
+    AutoImport({
+      resolvers: [ElementPlusResolver()], // element-plus
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()], // element-plus
+    })
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)) //实际的路径转换
