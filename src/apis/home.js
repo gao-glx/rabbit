9523ba57c6de7a81fae60c1e60c230f224b8c019
LixinGao <1187776469@qq.com>
2023-05-16 15:04:22 +0800

    使用panel实现新鲜好物

--- src/apis/home.js
+++ src/apis/home.js
@@ -1,4 +1,5 @@
 import httpInstance from "@/utils/http";
+//轮播图API
 export function getBannerAPI() {
   return httpInstance({
     url: 'home/banner',
@@ -6,4 +7,15 @@ export function getBannerAPI() {
     //   distributionSite: ""
     // }
   })
+}
+
+// 封装新鲜好物接口
+export const findNewAPI = () => {
+  return httpInstance({
+    url: '/home/new'
+  })
+}
+
+export const getHotAPI = () => {
+  return httpInstance('home/hot', 'get', {})
 }
