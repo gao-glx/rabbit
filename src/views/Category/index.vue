355d8cb9c443328a2687c161e256f6cf99fa93d9
LixinGao <1187776469@qq.com>
2023-05-16 19:12:10 +0800

    激活样式一级路由渲染

--- src/views/Category/index.vue
+++ src/views/Category/index.vue
@@ -3,7 +3,7 @@ import { ref, onMounted } from 'vue';
 import { useRoute } from 'vue-router';
 import { findTopCategoryAPI } from '@/apis/category'
 import { getBannerAPI } from '@/apis/home';
-
+import GoodsItem from '@/views/Home/components/GoodItems.vue'
 const categoryData = ref({})
 const route = useRoute()
 const getCategory = async (id) => {
@@ -46,6 +46,26 @@ onMounted(() => getBanner())
           </el-carousel-item>
         </el-carousel>
       </div>
+      <!-- 其他分类 -->
+      <div class="sub-list">
+        <h3>全部分类</h3>
+        <ul>
+          <li v-for="i in categoryData.children" :key="i.id">
+            <RouterLink to="/">
+              <img :src="i.picture" />
+              <p>{{ i.name }}</p>
+            </RouterLink>
+          </li>
+        </ul>
+      </div>
+      <div class="ref-goods" v-for="item in categoryData.children" :key="item.id">
+        <div class="head">
+          <h3>- {{ item.name }}-</h3>
+        </div>
+        <div class="body">
+          <GoodsItem v-for="good in item.goods" :goods="good" :key="good.id" />
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -64,18 +84,81 @@ onMounted(() => getBanner())
   }
 }
 
-.home-banner {
-  width: 1240px;
-  height: 500px;
-  margin: 0 auto;
-  // position: absolute;
-  // left: 0;
-  // top: 0;
-  // z-index: 98;
+.top-category {
+  h3 {
+    font-size: 28px;
+    color: #666;
+    font-weight: normal;
+    text-align: center;
+    line-height: 100px;
+  }
 
-  img {
-    width: 100%;
-    height: 500px;
+  .sub-list {
+    margin-top: 20px;
+    background-color: #fff;
+
+    ul {
+      display: flex;
+      padding: 0 32px;
+      flex-wrap: wrap;
+
+      li {
+        width: 168px;
+        height: 160px;
+
+
+        a {
+          text-align: center;
+          display: block;
+          font-size: 16px;
+
+          img {
+            width: 100px;
+            height: 100px;
+          }
+
+          p {
+            line-height: 40px;
+          }
+
+          &:hover {
+            color: $xtxColor;
+          }
+        }
+      }
+    }
+  }
+
+  .ref-goods {
+    background-color: #fff;
+    margin-top: 20px;
+    position: relative;
+
+    .head {
+      .xtx-more {
+        position: absolute;
+        top: 20px;
+        right: 20px;
+      }
+
+      .tag {
+        text-align: center;
+        color: #999;
+        font-size: 20px;
+        position: relative;
+        top: -20px;
+      }
+    }
+
+    .body {
+      display: flex;
+      justify-content: space-around;
+      padding: 0 40px 30px;
+    }
+  }
+
+  .bread-container {
+    padding: 25px 0;
   }
 }
 </style>
